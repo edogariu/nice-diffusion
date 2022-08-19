99e2abe2cae5df0ed0a0c70be04c6e7c7e9c94f6
Evan D <edogariu@princeton.edu>
2022-08-19 23:28:24 +0300

    refactored into a package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Evan Dogariu',
     author_email='eadogariu@aol.com / edogariu@princeton.edu',
-    packages=['nicediffusion'],
+    packages=['nicediffusion', 'nicediffusion.superresolution'],
     scripts=['scripts/sample.py', 'scripts/train.py'],
     url='http://pypi.python.org/pypi/nicediffusion/',
     license='LICENSE.txt',
